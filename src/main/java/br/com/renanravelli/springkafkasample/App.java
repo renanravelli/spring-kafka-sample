d995889a0f7f88817e5cf972c61713a7526642f0
Renan Ravelli <renan.fcamara@fornecedores.klabin.com.br>
2022-09-05 23:15:58 -0300

    Removendo annotation

--- src/main/java/br/com/renanravelli/springkafkasample/App.java
+++ src/main/java/br/com/renanravelli/springkafkasample/App.java
@@ -9,10 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.KafkaTemplate;
 
-@EnableKafka
 @SpringBootApplication
 public class App {
 
